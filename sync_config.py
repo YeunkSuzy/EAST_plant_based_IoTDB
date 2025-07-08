71e592abfef216c080154eeb92942d5ffbed34d8
YeungK <guangyang@mail.ustc.edu.cn>
2025-07-08 22:20:44 +0800

    Create sync_config.py

---
+++ sync_config.py
@@ -0,0 +1,20 @@
+# max_time_stamp of each_sync
+sync_max = 1000
+
+# max sync times 
+sync_time = 10
+
+# time interval of sync (seconds)
+sync_interval = 1
+
+# IoTDB config
+iotdb_ip = ""
+iotdb_user = ""
+iotdb_password = ""
+
+# mysql config
+mysql_ip = ""
+mysql_user = ""
+mysql_password = ""
+mysql_database = ""
+
