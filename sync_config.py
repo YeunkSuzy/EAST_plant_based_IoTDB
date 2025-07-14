ee4ea0542e3d1f4d6653f2518cb37887304f3352
YeungK <guangyang@mail.ustc.edu.cn>
2025-07-14 13:51:45 +0800

    Update sync_config.py

--- sync_config.py
+++ sync_config.py
@@ -1,20 +1,19 @@
 # max_time_stamp of each_sync
-sync_max = 1000
+sync_max = 10000
 
-# max sync times 
-sync_time = 10
+# max sync times (0 represent infinite loop)
+sync_time = 0
 
 # time interval of sync (seconds)
 sync_interval = 1
 
 # IoTDB config
-iotdb_ip = ""
-iotdb_user = ""
-iotdb_password = ""
+iotdb_ip = "202.127.205.115"
+iotdb_user = "root"
+iotdb_password = "cN9w4t%o3Ktb*#_&"
 
 # mysql config
-mysql_ip = ""
-mysql_user = ""
-mysql_password = ""
-mysql_database = ""
-
+mysql_ip = "202.127.205.89"
+mysql_user = "root"
+mysql_password = "cN9w4t%o3Ktb*#_&"
+mysql_database = "engdata"
